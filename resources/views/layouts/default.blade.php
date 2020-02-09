4f8df49911224899641e265014ab4fc6e9d067b8
zeyuan <1973547260@qq.com>
2020-02-09 12:53:37 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-  <!-- 第一个参数是该区块的变量名称，第二个参数是默认值 -->
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-  <!-- 用于显示 content 区块的内容 -->
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
