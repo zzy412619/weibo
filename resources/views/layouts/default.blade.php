da298ff9adf20adf07483045ecf1221ff2eb736d
zeyuan <1973547260@qq.com>
2020-02-09 11:10:09 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <!-- 第一个参数是该区块的变量名称，第二个参数是默认值 -->
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+  <!-- 用于显示 content 区块的内容 -->
+    @yield('content')
+  </body>
+</html>
