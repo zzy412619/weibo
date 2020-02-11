3baacf09703421ca9b0baa99c9f99d22fad81aa5
zeyuan <1973547260@qq.com>
2020-02-11 07:07:04 +0000

    增加 CSRF 验证

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
