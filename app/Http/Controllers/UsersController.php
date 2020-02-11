d6ccd7541074af3cf561741209a17ff18fc22fbf
zeyuan <1973547260@qq.com>
2020-02-11 08:05:23 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,7 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
-    
+
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -24,6 +24,16 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user]);
     }
+
 }
