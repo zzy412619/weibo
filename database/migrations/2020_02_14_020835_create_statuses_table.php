e20d35565c4a00e9766a0747c19d94a2672b7a85
zeyuan <1973547260@qq.com>
2020-02-14 02:26:56 +0000

    微博数据模型

---
+++ database/migrations/2020_02_14_020835_create_statuses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            // ->index()是索引，为了加快效率
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
