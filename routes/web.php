a4267f5cb82f43635503e6a54fb716e589526c4e
zeyuan <1973547260@qq.com>
2020-02-15 13:09:33 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,9 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 //创建微博和删除微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+//用户关注者列表和粉丝列表
+//显示用户的关注人列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+// 显示用户的粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
