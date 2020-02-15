276d2e6c58c424d62dfeb150e1c67eb24dc76ccb
zeyuan <1973547260@qq.com>
2020-02-15 13:32:24 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,8 +61,11 @@ class User extends Authenticatable
     // 将微博从数据库中取出
      public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
     // 关注的人和粉丝 多对多
     // 粉丝关系列表
